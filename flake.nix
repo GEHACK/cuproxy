0a1d145d5233d9204f2b84320ce1babbad4c8b96
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-04-17 13:03:54 +0200

    chore: add nix flaking and nix module

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "CUProxy – IPP proxy that stitches banner pages onto print jobs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "cuproxy";
+          src = ./.;
+
+          vendorHash = "sha256-tuSDQ1FAMn91GAkuoAQcPC1rX5sPl8MgijY5TrCV+ek=";
+
+          meta = with pkgs.lib; {
+            description = "IPP proxy that prepends/appends banner pages to print jobs";
+            homepage = "https://github.com/tuupke/cuproxy";
+            mainProgram = "cuproxy";
+            platforms = platforms.linux;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+          ];
+        };
+      }
+    )
+    // {
+      nixosModules.default = import ./nix/module.nix self;
+    };
+}
